92300373848d90355a4b4bab49b7c0e10c39e6a7
awei <lwei@meishubao.com>
2022-02-20 23:58:31 +0800

    unsafe-perm

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ NAME="auto-map"
 ROOT="/data/git/${NAME}"
 RUN="/data/wwwroot/${NAME}"
 
-npm i
+npm install --unsafe-perm
 npm run build
 mkdir -p ${RUN}
 cp -rf ${ROOT}/prod/index.html ${RUN}/index.html
